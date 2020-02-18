ecb1e63d92ee3c1ed8c4fd65a7c56651932bcd60
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 02:34:09 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,9 +11,8 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
+var thisIsCrazy = function (){
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
